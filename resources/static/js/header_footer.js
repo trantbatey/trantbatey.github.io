67f10af1c2baf359908cfe2a8eea52b9f4c47949
trantbatey <trantbatey@gmail.com>
2020-10-18 20:42:55 -0500

    Test draft of website on github.io

---
+++ resources/static/js/header_footer.js
@@ -0,0 +1,27 @@
+
+// function to use ajax to include the header a trailer templates
+
+(function () { // IFEE
+    "use strict"
+    $().ready(function () {
+
+        "use strict";
+
+        fetch("./header.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("header").innerHTML = data;
+            });
+
+        fetch("./footer.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("footer").innerHTML = data;
+            });
+
+    });
+})();
