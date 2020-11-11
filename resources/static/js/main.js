fee110fbcf5d7d294eb404e9d356aa0bc10cc7bb
trantbatey <trantbatey@gmail.com>
2020-11-11 03:42:46 -0600

    Have smaller video synced up with scroll bar.

--- resources/static/js/main.js
+++ resources/static/js/main.js
@@ -6,33 +6,53 @@
 
         "use strict";
 
-        // start background video on mouse down
-        let vidPlaying = false;
-        console.log("Got Here 01");
+        // define delay function
+        const delay = ms => new Promise(rs => setTimeout(rs, ms));
 
+        // create shared variables
+        let vidPlaying = false;
         let res;
-        let vid2;
+        let vid2, vidSB01;
         let scrollHeight;
         let frameNumber = 0; // start video at frame 0
-        let totalSeconds = 90; // 124 seconds at 30 fps
+        let maxFrame = 3720;
         let oldScrollTop = 0;
-        let step = 15;
+        let step;
+        let vidSBTimeLimit = 0;
+        let scrollCount = 0;
 
         function scrollHandler(event) {
 
-            // Use requestAnimationFrame for smooth playback
+            console.log("scrollTop: " + res[0].scrollTop + " vidSBTimeLimit: " + vidSBTimeLimit);
+            if (vidSBTimeLimit != 0) return;
+
+            // set step and direction
+            step = res[0].scrollTop - oldScrollTop;
             scrollHeight = res[0].scrollHeight - res.height();
             console.log("scrollHeight: " + scrollHeight);
-            if (res.scrollTop() === 0) {
+            console.log("scrollTop: " + res.scrollTop() + " oldScrollTop: " + oldScrollTop + " step: " + step);
+            if (res[0].scrollTop === 0) {
                 frameNumber = 1;
+                vidSBTimeLimit = 0;
+                console.log("res.scrollTop() <= 0, frameNumber: " + frameNumber);
             } else {
-                if (res[0].scrollTop - oldScrollTop > step) res[0].scrollTop = oldScrollTop + step;
-                else if (oldScrollTop - res[0].scrollTop > step ) res[0].scrollTop = oldScrollTop - step;
-                frameNumber = (res.scrollTop() / scrollHeight) * totalSeconds;
+                if (res.scrollTop() >= scrollHeight - 300) res[0].scrollTop = scrollHeight - 400;
+                if (step > 0) frameNumber += (1/7);
+                else if (step < 0) frameNumber -= (1/7);
+                console.log("else, frameNumber: " + frameNumber);
             }
             oldScrollTop = res[0].scrollTop;
-            console.log("scrollPlay: " + res.scrollTop() + " totalSeconds: " + totalSeconds + " Frame#: " + frameNumber);
-            vid2.currentTime = frameNumber;
+            console.log("scrollTop: " + res.scrollTop() + " step: " + step + " Frame#: " + frameNumber);
+            vid2.currentTime = (frameNumber*7) / 30;
+            console.log("vidSBTimeLimit: " + vidSBTimeLimit);
+            // if (vidSBTimeLimit === 0) {
+                vidSB01.currentTime = frameNumber;
+                vidSBTimeLimit = vidSB01.currentTime + (1 / 7);
+                vidSB01.play();
+            // } else {
+            //     if (step > 0) vidSBTimeLimit += (1/7);
+            //     else if (step < 0) vidSBTimeLimit -= (1/7);
+            // }
         }
 
         function setScrollHandler() {
@@ -48,6 +68,14 @@
             vid.play();
             vidPlaying = true;
             vid2 = $('#vid02')[0];
+            vidSB01 = $('#vidSB01')[0];
+            vidSB01.addEventListener("timeupdate", function(){
+                if(this.currentTime >= vidSBTimeLimit
+                    || this.duration >= vidSBTimeLimit) {
+                    vidSBTimeLimit = 0;
+                    this.pause();
+                }
+            });
 
             // set shared variables
             window.removeEventListener('mousedown', setScrollHandler);
